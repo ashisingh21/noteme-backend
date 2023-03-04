7b444a411801d1843f888b14cad4be1f6b4e1f71
Ashi Singh <ashi888032@gmail.com>
2023-03-04 16:22:32 +0530

    FIXES

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var cors = require('cors')
 app.use(cors())
 app.use(express.json())
 connectToMongo();
+const PORT = process.env.PORT || 5000;
 
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/note', require('./routes/note'))
 
-app.listen(5000);
+app.listen(PORT);
