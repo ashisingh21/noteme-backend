cd7499641667789b37d474e76aa68aea9e3a3663
Ashi Singh <ashi888032@gmail.com>
2023-03-04 17:35:29 +0530

    main fixes

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-const mongouri = process.env.MONGO_URI;
+
+const mongoose = require('mongoose')
+require('dotenv').config()
+const mongouri = process.env.MONGO_URL;
 
 const connectToMongo = () => {
-    mongoose.connect(mongouri, {
-        useNewUrlParser: true,
-        // useCreateIndex: true,
-        useUnifiedTopology: true,
-        // useFindAndModify: false
-    }).then(console.log("connected to mongo")).catch((err) => { console.log(err) })
+    mongoose.connect(mongouri,
+        { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log("connected to mongo")).catch((err) => { console.log(err) })
+
+
 }
 
 module.exports = connectToMongo;
--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const connectToMongo = require('./db');
 const app = express();
 var cors = require('cors')
+require('dotenv').config()
+
 app.use(cors())
 app.use(express.json())
 connectToMongo();
+
 const PORT = process.env.PORT || 5000;
 
 app.use('/api/auth', require('./routes/auth'))
